dd56890c69ffe5499c8a852648448243b2c17ad4
ChrisTran202 <Christran202@gmail.com>
2023-08-26 00:26:26 -0500

    Portfolio Update

--- index.js
+++
@@ -1,9 +0,0 @@
-function disableScrolling() {
-    setTimeout(function() {
-        document.body.style.overflow = 'hidden';
-    }, 1000);
-}
-  
-function enableScrolling() {
-    document.body.style.overflow = '';
-}
